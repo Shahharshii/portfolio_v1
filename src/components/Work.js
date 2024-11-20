778c27d6bdc1a0aab897024dde21bf3702124d96
Shahharshii <shahharshi694@gmail.com>
2024-11-20 11:43:53 +0530

    images update

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,7 @@ import { fadeIn } from '../variants'
 import notes from '../assets/notes.png'
 import movie from '../assets/movie.jpg'
 import dashboard from '../assets/dashboard.jpg'
+import guess from '../assets/guess.jpg'
 
 
 const Work = () => {
@@ -33,7 +34,7 @@ const Work = () => {
             {/* overlay */}
             <a href="https://guess-game-gray.vercel.app/" target="_blank">
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              <img className='group-hover:scale-125 transition-all duration-500' src={notes} alt="" />
+              <img className='group-hover:scale-125 transition-all duration-500' src={guess} alt="" />
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
                 <span className='text-gradient '>HTML and CSS</span>
               </div>
